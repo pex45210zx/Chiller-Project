7e1e7dfead378d7b12799dccf42fd840e5d28667
Siravat matarach <pex_der@outlook.co.th>
2023-09-10 16:36:20 +0700

    delete Router tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/HomePage/Home.jsx';
 import RegisterChiller from './components/RegisterPage/RegisterChiller.jsx';
 import YourChiller from './components/YourChillerPage/YourChiller.jsx';
@@ -7,16 +7,14 @@ import Delete from './components/DeletePage/Delete.jsx';
 
 function App() {
   return (
-    <Router>
-      <>
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register-chiller" element={<RegisterChiller />} />
         <Route path="/your-chiller" element={<YourChiller />} />
         <Route path="/delete" element={<Delete />} />
       </Routes>
-      </>
-    </Router>
+    </>
   );
 }
 
