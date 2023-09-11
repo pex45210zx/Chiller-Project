c6e727f4bf3ab4262dc360d5ddf39ac29d2d1b1b
Siravat matarach <pex_der@outlook.co.th>
2023-09-11 19:16:46 +0700

    lineLiff3

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom'; // Use the regular react-dom import
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Chiller-Project/'>
+    <BrowserRouter basename='/Chiller-Project'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
