3631ba8c9d7ef3393df850e8b229f0b94bf39f9f
Siravat matarach <pex_der@outlook.co.th>
2023-09-14 01:43:28 +0700

    submit function1

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/Chiller-Project/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
