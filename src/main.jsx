4f0415f07b3216c02f1a1c124f4ac3a0493c00d2
Siravat matarach <pex_der@outlook.co.th>
2023-09-14 01:50:37 +0700

    submit function2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // Use the regular react-dom import
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Chiller-Project/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- src/pages/RegisterPage/RegisterChiller.jsx
+++ src/pages/RegisterPage/RegisterChiller.jsx
@@ -33,54 +33,7 @@ function RegisterChiller() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    // Check if chillerId and chillerName are not empty
-    if (!chillerId || !chillerName) {
-      alert("Chiller ID and Chiller Name are required");
-      return;
-    }
-  
-    try {
-      // Fetch data from the Sheety API
-      const response = await fetch('https://api.sheety.co/313ba156926928db7871fc95577d36d9/chillerRegister/data');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data from Sheety API');
-      }
-      const data = await response.json();
-  
-      // Find the row with the matching chillerId
-      const chillerRow = data.find((row) => row.chillerId === chillerId);
-  
-      if (chillerRow) {
-        // Update the chillerName in column C
-        chillerRow.chillerName = chillerName;
-  
-        // Prepare the data to be sent back to the Google Sheet
-        const updatedData = {
-          data: data,
-        };
-  
-        // Update the data in the Google Sheet
-        const updateResponse = await fetch('https://api.sheety.co/313ba156926928db7871fc95577d36d9/chillerRegister/data', {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updatedData),
-        });
-  
-        if (updateResponse.ok) {
-          alert(`Chiller Name updated successfully for Chiller ID: ${chillerId}`);
-        } else {
-          throw new Error('Failed to update Chiller Name');
-        }
-      } else {
-        alert(`Chiller ID ${chillerId} not found in the spreadsheet`);
-      }
-    } catch (error) {
-      console.error(error);
-      alert('An error occurred while processing your request');
-    }
+
   };
 
   return (
