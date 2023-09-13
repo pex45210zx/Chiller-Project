3ebc45e7568e7dd0ad98174005f188a8ddc7d270
Siravat matarach <pex_der@outlook.co.th>
2023-09-14 00:04:45 +0700

    rollback

--- src/components/RegisterPage/RegisterChiller.jsx
+++ src/components/RegisterPage/RegisterChiller.jsx
@@ -5,7 +5,6 @@ import './RegisterChiller.css';
 import liff from '@line/liff';
 import { getProfileData } from '../localStorageUtils';
 import Header from '../header';
-import { loadSpreadsheetData, saveDataToGoogleSheet } from '../googleSheetsAPI.jsx';
 
 function RegisterChiller() {
   const [click, setClick] = useState(false);
@@ -35,38 +34,6 @@ function RegisterChiller() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { userId } = getProfileData();
-
-    try {
-      // Load data from Google Spreadsheet
-      const spreadsheetData = await loadSpreadsheetData();
-
-      // Find a match for the entered Chiller ID
-      const match = spreadsheetData.find((row) => row.ChillerID === chillerId);
-
-      if (match) {
-        // If a match is found, update the Chiller Name and User ID in the spreadsheet
-        const updatedData = spreadsheetData.map((row) => {
-          if (row.ChillerID === chillerId) {
-            row.ChillerName = chillerName; // Update Chiller Name
-            row.UserID = userId; // Replace 'userId' with the actual user ID
-          }
-          return row;
-        });
-
-        // Save the updated data back to the Google Spreadsheet
-        await saveDataToGoogleSheet(updatedData);
-
-        // Optionally, navigate to another page or show a success message
-        console.log('Chiller ID is correct. Registration successful.');
-        navigate('/register-chiller');
-      } else {
-        // If no match is found, display an error message
-        console.log('Chiller ID is incorrect. Please check and try again.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
   };
 
   return (
