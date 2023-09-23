f305a08548e5272e3b3bc6522efe07a15a8af7a8
Siravat matarach <pex_der@outlook.co.th>
2023-09-23 19:16:11 +0700

    after dev Home page

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 
 function LineChart({ chartData }) {
-  return (
-    <div>
-      <h2>Chiller Data</h2>
-      <Line
-        data={chartData}
-        options={{
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            x: {
-              type: 'time',
-              time: {
-                unit: 'day', // Customize the time unit as needed
-              },
-            },
-            y: {
-              title: {
-                display: true,
-                text: 'Temperature (°C)', // Customize the y-axis label
-              },
-            },
-          },
-        }}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Chiller Data</h2>
+            <Line
+                data={chartData}
+                options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales: {
+                        x: {
+                            type: 'time', // Set 'type' to 'time' for the x-axis
+                            time: {
+                                unit: 'day', // Customize the time unit as needed
+                            },
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'Temperature (°C)', // Customize the y-axis label
+                            },
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
 }
 
 export default LineChart;
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,12 +1,12 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../components/Header.css';
 import { useNavigate } from 'react-router-dom';
-import './Home.css'
+import './Home.css';
 import liff from '@line/liff';
 import { getProfileData } from '../../components/localStorageUtils';
 import Header from '../../components/Header.jsx';
 import { fetchChillerData } from '../../components/googleSheetsApi';
-
+import LineChart from '../../components/LineChart';
 
 function Home() {
   const [click, setClick] = useState(false);
@@ -14,7 +14,8 @@ function Home() {
   const { profilePicture, displayName } = getProfileData();
   const [chillerOptions, setChillerOptions] = useState([]);
   const [selectedChiller, setSelectedChiller] = useState('');
-
+  const [chillerId, setChillerId] = useState(''); // Track selected chillerId
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] }); // Data for the chart
 
   useEffect(() => {
     async function fetchData() {
@@ -29,22 +30,83 @@ function Home() {
 
     fetchData();
   }, []);
-
+  
   const handleClick = () => {
     setClick(!click);
   };
-
-  const handleLogout = () => {
+const handleLogout = () => {
     liff.logout();
     navigate('/');
     console.log('clicked logout');
   };
 
   const handleChillerChange = (e) => {
-    setSelectedChiller(e.target.value);
-    // Reset temperature values when the chiller is changed
+    const selectedChillerName = e.target.value;
+    setSelectedChiller(selectedChillerName);
+
+    // Find the chillerId based on the selected chiller name
+    const selectedChillerData = chillerOptions.find(
+      (chiller) => chiller.chillerName === selectedChillerName
+    );
+
+    if (selectedChillerData) {
+      const chillerId = selectedChillerData.chillerId;
+      setChillerId(chillerId);
+
+      // Fetch data for the selected chillerId from Google Sheets API
+      fetchChillerDataFromGoogleSheets(chillerId);
+    } else {
+      // Reset chart data if no chiller is selected
+      setChartData({ labels: [], datasets: [] });
+    }
   };
 
+  // Function to fetch chiller data from Google Sheets API based on chillerId
+  const fetchChillerDataFromGoogleSheets = async (chillerId) => {
+    try {
+      // Make a request to your Google Sheets API endpoint with chillerId
+      // Replace 'YOUR_API_ENDPOINT' with the actual endpoint
+      const response = await fetch(`https://api.sheety.co/313ba156926928db7871fc95577d36d9/chillerRegister/data?chillerId=${chillerId}`);
+
+      if (!response.ok) {
+        console.error('Failed to fetch chiller data', response.status, await response.text());
+        return;
+      }
+
+      const responseData = await response.json();
+      // Parse the response data and prepare it for the line chart
+      const parsedData = parseChillerData(responseData);
+      setChartData(parsedData);
+    } catch (error) {
+      console.error('An error occurred while fetching chiller data:', error);
+    }
+  };
+
+  // Function to parse chiller data and prepare it for the line chart
+  const parseChillerData = (data) => {
+    // Implement parsing logic here based on your data structure
+    // Create an object with 'labels' and 'datasets' properties
+    // 'labels' should contain an array of timestamps, and 'datasets' should contain temperature data
+    // Format your data according to the format expected by the LineChart component
+
+    const labels = []; // Array of timestamps
+    const temperatureData = []; // Array of temperature values
+
+    // Implement the logic to extract data from 'data' and populate 'labels' and 'temperatureData' arrays
+
+    return {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Temperature (°C)',
+          data: temperatureData,
+          borderColor: 'rgb(75, 192, 192)',
+          borderWidth: 2,
+          fill: false,
+        },
+      ],
+    };
+  };
 
   return (
     <div className="header">
@@ -70,7 +132,7 @@ function Home() {
         </div>
       </div>
       <div className="line-chart">
-
+        <LineChart chartData={chartData} />
       </div>
     </div>
   );
