b64b781aaffd8a75f5e62e5a7ba83e715f0d4b8b
Siravat matarach <pex_der@outlook.co.th>
2023-09-25 20:02:48 +0700

    before dev Home page

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -39,45 +39,9 @@ const handleLogout = () => {
   };
 
   const handleChillerChange = (e) => {
-    const selectedChillerName = e.target.value;
-    setSelectedChiller(selectedChillerName);
+    setSelectedChiller(e.target.value);
 
-    // Find the chillerId based on the selected chiller name
-    const selectedChillerData = chillerOptions.find(
-      (chiller) => chiller.chillerName === selectedChillerName
-    );
-
-    if (selectedChillerData) {
-      const chillerId = selectedChillerData.chillerId;
-      setChillerId(chillerId);
-
-      // Fetch data for the selected chillerId from Google Sheets API
-      fetchChillerDataFromGoogleSheets(chillerId);
-    } else {
-      // Reset chart data if no chiller is selected
-      setChartData({ labels: [], datasets: [] });
-    }
-  };
-
-  // Function to fetch chiller data from Google Sheets API based on chillerId
-  const fetchChillerDataFromGoogleSheets = async (chillerId) => {
-    try {
-      // Make a request to your Google Sheets API endpoint with chillerId
-      // Replace 'YOUR_API_ENDPOINT' with the actual endpoint
-      const response = await fetch(`https://api.sheety.co/313ba156926928db7871fc95577d36d9/chillerRegister/data?chillerId=${chillerId}`);
-
-      if (!response.ok) {
-        console.error('Failed to fetch chiller data', response.status, await response.text());
-        return;
-      }
-
-      const responseData = await response.json();
-      // Parse the response data and prepare it for the line chart
-      const parsedData = parseChillerData(responseData);
-      setChartData(parsedData);
-    } catch (error) {
-      console.error('An error occurred while fetching chiller data:', error);
-    }
+    
   };
 
   return (
@@ -103,8 +67,7 @@ const handleLogout = () => {
           </select>
         </div>
       </div>
-      <div className="line-chart">
-        
+      <div className="chiller-status">
       </div>
     </div>
   );
